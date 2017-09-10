e3f52b60673b5cf52602155b134726040eb15ad4
Stefan <stadolf@gmail.com>
2017-09-11 00:07:31 +0200

    some details for the elmariachi card

--- src/elmariachi111/index.js
+++ src/elmariachi111/index.js
@@ -4,6 +4,11 @@ const tpl = require('./card.hbs')
 export class Card {
     render($el) {
         const markup = tpl({name: 'your name'});
+        
         $el.html(markup);
+        $el.addClass('zaxis');
+        $el.on('click', () => {
+            $el.toggleClass('zaxis').toggleClass('yaxis');
+        })
     }
 }
