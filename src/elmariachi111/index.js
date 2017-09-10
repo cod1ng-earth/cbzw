e0208373f6f5ffc937c071f9f62b4712359a6fb1
Stefan <stadolf@gmail.com>
2017-09-10 23:12:05 +0200

    handlebars basic card interface

---
+++ src/elmariachi111/index.js
@@ -0,0 +1,9 @@
+require('./card.scss');
+const tpl = require('./card.hbs')
+
+export class Card {
+    render($el) {
+        const markup = tpl({name: 'your name'});
+        $el.html(markup);
+    }
+}
