b213d3683581f53f45ee53d15e08dd2a29dbce41
Stefan <stadolf@gmail.com>
2017-09-17 20:15:56 +0200

    replaced http-server with express to support simple backend services elmariachi: use bitcoin exchange rate service

--- src/elmariachi111/index.js
+++ src/elmariachi111/index.js
@@ -1,14 +1,49 @@
 require('./card.scss');
 const tpl = require('./card.hbs')
 
+const COINDESK_API = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+
 export default class Card {
     render($el) {
-        const markup = tpl({name: 'Elmariachi111'});
-        
-        $el.html(markup);
-        $el.addClass('zaxis');
-        $el.on('click', () => {
-            $el.toggleClass('zaxis').toggleClass('yaxis');
+        this.$el = $el;
+        this.$el.on('click', () => {
+            this.$el.toggleClass('zaxis').toggleClass('yaxis');
+        });
+        this.$el.addClass('zaxis');
+
+        this.fetchBitcoinRate().then(rate => {
+            this.bitcoinRate = rate;
+            this._render();
+        }).catch(() => {
+            console.log('ups');
         })
     }
+
+    _render() {
+        const markup = tpl({name: 'Elmariachi111', bitcoin: this.bitcoinRate});
+        this.$el.html(markup);
+        this._events();
+    }
+
+    _events() {
+        this.$el.find('a[rel=refresh]').on('click', (evt) => {
+            evt.stopPropagation();
+            evt.preventDefault();
+            this.render(this.$el);
+            return false;
+        });
+    }
+
+    fetchBitcoinRate() {
+        return new Promise((resolve, reject) => {
+            fetch(COINDESK_API).then(response => {
+                if (response.ok) {
+                    resolve(response.json());
+                } else {
+                    reject();
+                }
+            });
+        });
+    }
+
 }
