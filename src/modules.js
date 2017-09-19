d5b7ba3381d20790d9b1d97ff16898bbaf5f6d59
Stefan <stadolf@gmail.com>
2017-09-19 22:25:46 +0200

    added a new card

--- src/modules.js
+++ src/modules.js
@@ -1,5 +1,7 @@
+import * as theartistformerlyknownaselmariachi from './theartistformerlyknownaselmariachi';
 import * as elmariachi111 from './elmariachi111';
 
 export {
     elmariachi111
+	,theartistformerlyknownaselmariachi 
 }
