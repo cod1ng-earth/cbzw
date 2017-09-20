926ae9298b9234af968ba0da7cfdc58c3241764c
Stefan <stadolf@gmail.com>
2017-09-20 20:35:31 +0200

    added remaining cards

--- src/modules.js
+++ src/modules.js
@@ -3,10 +3,11 @@ import * as konstantin from './konstantin';
 import * as bazoo from './bazoo';
 import * as vollbio from './vollbio';
 import * as elmariachi111 from './elmariachi111';
-
+import * as rene from './rene';
 export {
     elmariachi111
 	,konstantin 
-	,bazoo 
+	,rene
+	,bazoo
 	,vollbio 
 }
